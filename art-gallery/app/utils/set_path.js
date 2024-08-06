0247520bcefdda33da717af3540cf37f3e191f25
Rounak Agrawal <74702699+userrounakk@users.noreply.github.com>
2024-08-06 21:48:07 +0530

    fix: auth image path corrected

--- art-gallery/app/utils/set_path.js
+++ art-gallery/app/utils/set_path.js
@@ -1,6 +1,9 @@
 const setPath = async (req, res, next) => {
   const baseUrl = req.baseUrl;
   req.dir = `public/uploads/${baseUrl}`;
+  if (baseUrl == "auth") {
+    req.dir = "public/uploads/users";
+  }
   next();
 };
 
